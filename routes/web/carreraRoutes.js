82a3fb4f1c928773aa05d6b4719860e85b5e18ff
rmMarioAlberto <rmmarioalberto08@gmail.com>
2025-02-27 22:02:46 -0600

    correcion rutas

--- routes/web/carreraRoutes.js
+++ routes/web/carreraRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const carreraCrud = require('../../controllers/web/carreraCrud');
 
-router.post('/carrea', carreraCrud.getCarrera);
-router.post('/carreaById', carreraCrud.getCarreraById);
+router.post('/carrera', carreraCrud.getCarrera);
+router.post('/carreraById', carreraCrud.getCarreraById);
 router.post('/addCarrera', carreraCrud.addCarrera);
 router.put('/updateCarrera', carreraCrud.updateCarrera);
-router.post('/findCarrea', carreraCrud.searchCarrera)
+router.post('/findCarrera', carreraCrud.searchCarrera)
 
 module.exports = router;
