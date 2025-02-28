49cbe1fd57878f741c9e70bc46cf1a232ddfe9fc
rmMarioAlberto <rmmarioalberto08@gmail.com>
2025-02-27 22:01:12 -0600

    Crud de carreras

--- routes/web/carreraRoutes.js
+++ routes/web/carreraRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const carreraCrud = require('../../controllers/web/carreraCrud');
 
-router.get('/carrera', carreraCrud.getCarrera);
+router.post('/carrea', carreraCrud.getCarrera);
+router.post('/carreaById', carreraCrud.getCarreraById);
+router.post('/addCarrera', carreraCrud.addCarrera);
+router.put('/updateCarrera', carreraCrud.updateCarrera);
+router.post('/findCarrea', carreraCrud.searchCarrera)
 
 module.exports = router;
