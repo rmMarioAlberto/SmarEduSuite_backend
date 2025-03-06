25ba8021b64c6bda352300f9d97d45f63e248db3
rmMarioAlberto <rmmarioalberto08@gmail.com>
2025-03-05 18:28:31 -0600

    apis login movil

--- controllers/movil/authController.js
+++ controllers/movil/authController.js
@@ -2,7 +2,6 @@ const jwtConfig = require('../../config/jwtConfig');
 const db = require('../../config/pg');
 
 exports.loginMovil = (req, res) => {
-
     const { correo, contra } = req.body;
 
     if (!correo) {
@@ -14,16 +13,14 @@ exports.loginMovil = (req, res) => {
     }
 
     const query = 'SELECT id, nombre, "apellidoMa", "apellidoPa", contra, correo, tipo, status, huella, "idGoogle", "idGrupo", token_movil FROM usuario WHERE correo = $1 AND status = 1';
-    const query2 = 'UPDATE usuario SET token_movil = $1 WHERE id = $2';
 
     db.query(query, [correo], (err, results) => {
         if (err) {
-            return res.status(500).json({ message: 'Error en el servidor : 1' });
+            return res.status(500).json({ message: 'Error en el servidor' });
         }
         if (results.rows.length === 0) {
-            return res.status(404).json({ message: 'Usuario no encontrado : 1' });
+            return res.status(404).json({ message: 'Usuario no encontrado' });
         }
-
 
         const user = results.rows[0];
         const filteredUser = {
@@ -46,33 +43,46 @@ exports.loginMovil = (req, res) => {
         if (user.tipo === 3) {
             return res.status(403).json({ message: 'No tienes permisos para acceder a esta aplicación' });
         }
-        // Utilizar la función createToken en lugar de jwt.sign
-        const tokenMovil = jwtConfig.createToken(user.id, user.correo);
 
-        if (user.contra === null) {
-            db.query(query2, [tokenMovil, user.id], (err, results) => {
-                if (err) {
-                    return res.status(500).json({ message: 'Error en el servidor : 2' });
-                }
-                if (results.rowCount === 0) {
-                    return res.status(404).json({ message: 'Usuario no encontrado : 2' });
-                }
-                return res.status(300).json({ message: "Primer login", user: filteredUser, tokenMovil });
-            });
-        } else {
-            if (user.contra !== contra) {
-                return res.status(401).json({ message: 'Contraseña incorrecta : 1' });
+        // Validar el token actual
+        jwtConfig.validateTokenMovil(user.id, user.token_movil, (result) => {
+            if (result.valid) {
+                return res.status(409).json({ message: 'Ya hay una sesión activa' });
             }
-            db.query(query2, [tokenMovil, user.id], (err, results) => {
-                if (err) {
-                    return res.status(500).json({ message: 'Error en el servidor : 2' });
+
+            // Si el token no es válido o ha expirado, proceder con el login
+            if (user.contra === null) {
+                const tokenMovil = jwtConfig.createToken(user.id, user.correo);
+                const query2 = 'UPDATE usuario SET token_movil = $1 WHERE id = $2';
+
+                db.query(query2, [tokenMovil, user.id], (err, results) => {
+                    if (err) {
+                        return res.status(500).json({ message: 'Error en el servidor' });
+                    }
+                    if (results.rowCount === 0) {
+                        return res.status(404).json({ message: 'Usuario no encontrado' });
+                    }
+                    return res.status(300).json({ message: "Primer login", user: filteredUser, tokenMovil });
+                });
+            } else {
+                if (user.contra !== contra) {
+                    return res.status(401).json({ message: 'Contraseña incorrecta' });
                 }
-                if (results.rowCount === 0) {
-                    return res.status(404).json({ message: 'Usuario no encontrado : 2' });
-                }
-                return res.status(200).json({ message: 'Login exitoso', user: filteredUser, tokenMovil });
-            });
-        }
+
+                const tokenMovil = jwtConfig.createToken(user.id, user.correo);
+                const query2 = 'UPDATE usuario SET token_movil = $1 WHERE id = $2';
+
+                db.query(query2, [tokenMovil, user.id], (err, results) => {
+                    if (err) {
+                        return res.status(500).json({ message: 'Error en el servidor' });
+                    }
+                    if (results.rowCount === 0) {
+                        return res.status(404).json({ message: 'Usuario no encontrado' });
+                    }
+                    return res.status(200).json({ message: 'Login exitoso', user: filteredUser, tokenMovil });
+                });
+            }
+        });
     });
 };
 
