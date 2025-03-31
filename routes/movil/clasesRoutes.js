130b48568637d9949c8188deb434e12fc3de1401
Alberto <rmmarioalberto08@gmail.com>
2025-03-31 14:21:54 -0600

    Correccion de las mmds de sergio

--- routes/movil/clasesRoutes.js
+++ routes/movil/clasesRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router();
 const clasesController = require('../../controllers/movil/clasesController');
 
-routes.post('/claseActualTeacher', clasesController.getClasesActivasMaestro);
+routes.post('/clasesActivasMaestro', clasesController.getClasesActivasMaestro);
 routes.post('/horarioMaestro', clasesController.getHorarioMaestro);
 
 routes.post('/clasesActivasAlumno', clasesController.getClasesActivasDiaAlumno);
